1f7e2528236b4ed6304bb9dd977738d4a5cb8c31
Alex_Calatayud <alexcalatayud9.@gmail.com>
2025-03-10 01:46:14 -0400

    Subiendo archivos esenciales del proyecto

---
+++ Source/Bomberman2025/Private/Bob.cpp
@@ -0,0 +1,31 @@
+﻿
+
+
+#include "Bob.h"
+
+ABob::ABob()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+void ABob::BeginPlay()
+{
+	Super::BeginPlay();
+	SetLifeSpan(10);
+
+}
+void ABob::EndPlay(const EEndPlayReason::Type EndPlayReason) {
+	Super::EndPlay(EndPlayReason);
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 7.0f, FColor::Green, TEXT("💀 ¡Bob ha sido eliminado!"));
+	}
+}
+
+void ABob::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+
---
+++ Source/Bomberman2025/Private/MyGameModeBase.cpp
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Bloque.h"
+#include "MyGameModeBase.h"
+
+void AMyGameModeBase::BeginPlay()
+{
+	Super::BeginPlay();
+	GEngine->AddOnScreenDebugMessage(-1, -1, FColor::Red,
+		TEXT("Aparecio el ACTOOR"));
+	FTransform SpawnLocation;
+	GetWorld()->SpawnActor<ABloque>(
+		ABloque::StaticClass(), SpawnLocation);
+	FTimerHandle Timer;
+	SpawnedActor = GetWorld()->SpawnActor<ABloque>
+		(ABloque::StaticClass(), SpawnLocation);
+	GetWorldTimerManager().SetTimer(Timer, this,
+		&AMyGameModeBase::DestroyActorFunction, 10.0f, false);
+};
+void AMyGameModeBase::DestroyActorFunction()
+{
+	if (SpawnedActor != nullptr)
+	{
+		SpawnedActor->Destroy();
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red,
+			TEXT("¡Actor eliminado correctamente!"));
+		GEngine->AddOnScreenDebugMessage(-1, 7.0f, FColor::Green, TEXT(" Bob ha muerto xd"));
+	}
+}
