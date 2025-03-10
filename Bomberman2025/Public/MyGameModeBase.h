df894f66c0a05925c171b751b35dcf72082aed55
Alex_Calatayud <alexcalatayud9.@gmail.com>
2025-03-10 01:56:35 -0400

    Primer commit del proyecto Bomberman

---
+++ Bomberman2025/Public/MyGameModeBase.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyGameModeBase.generated.h"
+/**
+ * 
+ */
+class ABloque;
+UCLASS()
+class BOMBERMAN2025_API AMyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	public:
+    virtual void BeginPlay() override;
+public:
+
+	UPROPERTY()
+	ABloque* SpawnedActor;
+	UFUNCTION()
+	void DestroyActorFunction();
+};
