033d2b3bf1ff6bb3f2328a8bdac1092fd2275998
Alex_Calatayud <alexcalatayud9.@gmail.com>
2025-03-19 21:42:23 -0400

    Corrigiendo .gitignore y agregando archivos faltantes

--- Source/Bomberman2025/Bomberman2025.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Bomberman2025.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Bomberman2025, "Bomberman2025" );
- 
